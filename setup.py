e1e0f440906402655cd634cb9f9ad05ccfd2ce9c
Kashu <39104615+Kashu7100@users.noreply.github.com>
2020-01-02 07:19:30 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(name='PyChess',
-      version='0.0.1',
+      version='0.1.1',
       description='PyChess is a pure Python chess library with move generation and move validation as well as GUI',
-      author='Kashu',
+      author='Kashu Yamazaki',
       author_email='echo_six0566@yahoo.co.jp',
       url='https://github.com/Kashu7100/PyChess',
       packages=find_packages(),
